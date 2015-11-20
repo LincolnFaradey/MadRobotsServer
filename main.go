3564eb5ea4a2e8fd54d2fdd00abfdad25d96244d
lincolnfaradey <iphoenix179@gmail.com>
2015-11-20 22:14:32 +0300

    Player struct

--- main.go
+++ main.go
@@ -6,36 +6,36 @@ import (
 	"fmt"
 )
 
-var activePlayers = make(map[string]*websocket.Conn)
+var activePlayers = make(map[string] Player)
 
 type Player struct {
 	Name string `json:"name"`
 	X float32 `json:"x"`
 	Y float32 `json:"y"`
+	Socket *websocket.Conn `json:"–"`
 }
 
 func Game(ws *websocket.Conn)  {
 	defer ws.Close()
 
 	var player Player
-
 	for {
 		if err := websocket.JSON.Receive(ws, &player); err != nil {
 			panic(err)
 			return
 		}
+		player.Socket = ws
 
-		activePlayers[player.Name] = ws
 		fmt.Println(player)
 		for k, v := range activePlayers {
 			if k == player.Name { continue }
 
-			if err := websocket.JSON.Send(v, player); err != nil {
+			if err := websocket.JSON.Send(v.Socket, player); err != nil {
 				delete(activePlayers, k)
 
 				go func() {
-					for _, sock := range activePlayers {
-						websocket.JSON.Send(sock, &Player{
+					for _, p := range activePlayers {
+						websocket.JSON.Send(p.Socket, &Player{
 							Name: k,
 							X: 0,
 							Y: 0,
